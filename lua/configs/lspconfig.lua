d3bb58514eef3312be3d6676ce653234417115df
C0d3-5t3w <stewy1994@gmail.com>
2025-04-21 07:19:25 -0700

    Update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "lua_ls", "ts_ls" }
+local servers = { "html", "cssls", "gopls", "lua_ls", "ts_ls", "zls", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
